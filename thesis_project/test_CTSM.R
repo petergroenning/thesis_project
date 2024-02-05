ca05e3d9b3883ff483dc3368ca5d303de63f45a6
Peter Grønning <s183922@student.dtu.dk>
2024-02-05 10:34:49 +0100

    Setting up CTSMR

---
+++ thesis_project/test_CTSM.R
@@ -0,0 +1,116 @@
+library(ctsmrTMB)
+library(ggplot2)
+library(patchwork)
+
+############################################################
+# Data simulation
+############################################################
+
+# Simulate data using Euler Maruyama
+set.seed(10)
+pars = c(theta=10, mu=1, sigma_x=1, sigma_y=1e-2)
+# 
+dt.sim = 1e-3
+t.sim = seq(0,1,by=dt.sim)
+dw = rnorm(length(t.sim)-1,sd=sqrt(dt.sim))
+x = 3
+for(i in 1:(length(t.sim)-1)) {
+  x[i+1] = x[i] + pars[1]*(pars[2]-x[i])*dt.sim + pars[3]*dw[i]
+}
+
+# Extract observations and add noise
+dt.obs = 1e-2
+t.obs = seq(0,1,by=dt.obs)
+y = x[t.sim %in% t.obs] + pars[4] * rnorm(length(t.obs))
+
+# Create data
+.data = data.frame(
+  t = t.obs,
+  y = y
+)
+
+############################################################
+# Model creation and estimation
+############################################################
+
+# Create model object
+obj = ctsmrTMB$new()
+
+# Set name of model (and the created .cpp file)
+obj$set_modelname("ornstein_uhlenbeck")
+
+# Set path where generated C++ files are saved.
+# This will create a cppfiles folder in your current working directory if it doesnt exist
+obj$set_cppfile_directory(".cppfiles")
+
+# Add system equations
+obj$add_systems(
+  dx ~ theta * (mu-x) * dt + sigma_x*dw
+)
+
+# Add observation equations
+obj$add_observations(
+  y ~ x
+)
+
+# Set observation equation variances
+obj$add_observation_variances(
+  y ~ sigma_y^2
+)
+
+# Specify algebraic relations
+obj$add_algebraics(
+  theta   ~ exp(logtheta),
+  sigma_x ~ exp(logsigma_x),
+  sigma_y ~ exp(logsigma_y)
+)
+
+# Specify parameter initial values and lower/upper bounds in estimation
+obj$add_parameters(
+  logtheta    = log(c(init = 1, lower=1e-5, upper=50)),
+  mu          = c(init=1.5, lower=0, upper=5),
+  logsigma_x  = log(c(init= 1e-1, lower=1e-10, upper=10)),
+  logsigma_y  = log(c(init=1e-1, lower=1e-10, upper=10))
+)
+
+# Set initial state mean and covariance
+obj$set_initial_state(x[1], 1e-1*diag(1))
+
+# Carry out estimation using extended kalman filter method with stats::nlminb as optimizer
+fit <- obj$estimate(data=.data, method="ekf", ode.solver="rk4", use.hessian=TRUE)
+
+fit$states
+
+# Check parameter estimates against truth
+p0 = fit$par.fixed
+cbind(c(exp(p0[1]),p0[2],exp(p0[3]),exp(p0[4])), pars)
+
+# Create plot of one-step predictions, simulated states and observations
+t.est = fit$states$mean$prior$t
+x.mean = fit$states$mean$prior$x
+x.sd = fit$states$sd$prior$x
+plot1 = ggplot() +
+  geom_ribbon(aes(x=t.est, ymin=x.mean-2*x.sd, ymax=x.mean+2*x.sd),fill="grey", alpha=0.9) +
+  geom_line(aes(x=t.est, x.mean),col="steelblue",lwd=1) +
+  geom_line(aes(x=t.sim,y=x)) + 
+  geom_point(aes(x=t.obs,y=y),col="tomato",size=2) +
+  theme_minimal()
+
+# Predict to obtain k-step-ahead predictions to see model forecasting ability
+pred = obj$predict(data=.data, k.ahead=10, method="ekf", ode.solver="rk4")
+
+# Create plot all 10-step predictions against data
+pred10step = pred[pred$k.ahead==10,]
+plot2 = ggplot() +
+  geom_point(aes(x=t.obs,y=y),color="steelblue") +
+  geom_point(aes(x=pred10step$t.j,pred10step$x),color="tomato") +
+  geom_errorbar(aes(x=pred10step$t.j, 
+                    ymin=pred10step$x-2*sqrt(pred10step$var.x), 
+                    ymax=pred10step$x+2*sqrt(pred10step$var.x)),color="tomato") +
+  theme_minimal()
+
+# Draw both plots
+plot1 / plot2
+
+# Plot one-step-ahead residual analysis
+plot(fit)
